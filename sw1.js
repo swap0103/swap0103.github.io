e593e356c784d8c709a19a3ca6709849b4a968ba
swap0103 <81018131+swap0103@users.noreply.github.com>
2021-06-14 13:49:10 +0530

    Create sw1.js

---
+++ sw1.js
@@ -0,0 +1,24 @@
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('v1').then((cache) => {
+      return cache.addAll([
+        './',
+        './VaccineFinder.html',
+        './Test.html',
+        './style.css',
+        './master.jss',
+        './main.js'
+      ]);
+    })
+  );
+});
+self.addEventListener('fetch', function(event) {
+ console.log(event.request.url);
+});
+
+event.respondWith(
+   caches.match(event.request).then(function(response) {
+     return response || fetch(event.request);
+   })
+ );
+});
