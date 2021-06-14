8866832dc9a84c1f11fb651fb9d657d5be5f339c
swap0103 <81018131+swap0103@users.noreply.github.com>
2021-06-14 13:59:11 +0530

    Update sw1.js

--- sw1.js
+++ sw1.js
@@ -6,7 +6,7 @@ self.addEventListener('install', (event) => {
         './VaccineFinder.html',
         './Test.html',
         './style.css',
-        './master.jss',
+        './master.js',
         './main.js'
       ]);
     })
