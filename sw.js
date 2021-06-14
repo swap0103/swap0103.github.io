0668dd4e759c819a9fefbf84d8f3c7bb0ccd6f5c
swap0103 <81018131+swap0103@users.noreply.github.com>
2021-06-14 14:07:37 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,23 @@
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('v1').then((cache) => {
+      return cache.addAll([
+        './',
+        './VaccineFinder.html',
+        './Test.html',
+        './style.css',
+        './master.js',
+        './main.js'
+      ]);
+    })
+  );
+});
+self.addEventListener('fetch', function(event) {
+ console.log(event.request.url);
 
+event.respondWith(
+   caches.match(event.request).then(function(response) {
+     return response || fetch(event.request);
+   })
+ );
+});
